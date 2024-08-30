69a76fc026cc09dbbc72ffa9558f7b26f9d31c17
Gmicheletti <guilherme_micheletti@hotmail.com>
2024-08-29 21:02:39 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ export default function App() {
 
   return (
     <section className='bg-black'>
-      <img src="src/assets/img/logotransparent.svg" className='logo'/>
+      <img src="./img/logotransparent.svg" className='logo'/>
     
       <video id="heroVideo" autoPlay loop muted>
-            <source src="src/assets/video/verde.mp4" type="video/mp4" />
+            <source src="./video/verde.mp4" type="video/mp4" />
       </video>
     </section>
   )
