7b4072766ec824a7d2415a113ad39a9ef0fbb961
Gmicheletti <guilherme_micheletti@hotmail.com>
2024-09-10 11:22:05 -0300

    login

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -5,11 +5,11 @@ export  function Login() {
     return (
       <section id='sectionLogin'>
         <div className='loginLeft'>
-            <img id='loginBG' src="src/assets/img/bgLogin.jpg" alt="" />
-            <img id='loginLogoG' src="src/assets/img/agroG.svg" alt="" />
+            <img id='loginBG' src="./img/bgLogin.jpg" alt="" />
+            <img id='loginLogoG' src="./img/agroG.svg" alt="" />
         </div>
         <div  className='loginRight'>
-            <img src="src/assets/img/logoAC.svg" alt="" />
+            <img src="./img/logoAC.svg" alt="" />
             <input className='inputStd' placeholder='email' type="email" />
             <input className='inputStd' placeholder='senha' type="password" />
             <button  className='btn1'>Entrar</button>
