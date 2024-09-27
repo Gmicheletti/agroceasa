174cbea806cd383dfcc2d89f7fb6c4835dfc3938
Gmicheletti <guilherme_micheletti@hotmail.com>
2024-09-27 13:44:41 -0300

    vercel insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import { Login } from './login/Login'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
 
   return (
     <section id='app'>
-
       <Login />
+      <Analytics />
     </section>
   )
 }
